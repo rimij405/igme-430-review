c87062317ee8450034465df9e726f14a2583dcf4
Ian Effendi <effendiian@gmail.com>
2020-04-22 07:58:52 -0400

    Add state to Library class

--- exercises/3_react-essentials/react-is-fun-01/src/index.js
+++ exercises/3_react-essentials/react-is-fun-01/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 const bookList = [
+  {"title": "Hunger", "author": "Roxane Gay", "pages": 320 },
   {"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260 },
   {"title": "White Teeth", "author": "Zadie Smith", "pages": 480 },
   {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304 }
@@ -17,16 +18,36 @@ const Book = ({title, author, pages}) => {
   );
 };
 
-const Library = ({books}) => {
-  return (
-    <div>
-      {books.map((book, id) => {
-        return(
-          <Book key={book.title} title={book.title} author={book.author} pages={book.pages} />
-        );
-      })} 
-    </div>
-  );
+class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    const {books} = this.props;
+    return (
+      <div>
+        <h1>The library is {this.state.open ? 'open' : 'closed'}.</h1>
+        <button onClick={this.toggle}>Change</button>
+        {books.map((book, id) => {
+          return(
+            <Book key={id} title={book.title} author={book.author} pages={book.pages} />
+          );
+        })} 
+      </div>
+    );
+  };
 };
 
 render(
