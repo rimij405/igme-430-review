054ac14bd0daecf4d0c113b3a0c934e6fc88f416
Ian Effendi <effendiian@gmail.com>
2020-04-23 01:28:34 -0400

    Default properties for classes and functions

--- exercises/3_react-essentials/react-is-fun-01/src/index.js
+++ exercises/3_react-essentials/react-is-fun-01/src/index.js
@@ -1,126 +1,107 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-// const bookList = [
-//   {"title": "Hunger", "author": "Roxane Gay", "pages": 320 },
-//   {"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260 },
-//   {"title": "White Teeth", "author": "Zadie Smith", "pages": 480 },
-//   {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304 }
-// ];
+const bookList = [
+  {"title": "Default Test" },
+  {"title": "Hunger", "author": "Roxane Gay", "pages": 320 },
+  {"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260 },
+  {"title": "White Teeth", "author": "Zadie Smith", "pages": 480 },
+  {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304 }
+];
 
-// const Book = ({title, author, pages, freeBookmark}) => {
-//   return (
-//     <section>
-//       <h2>{title}</h2>
-//       <p>by: {author}</p>
-//       <p>Pages: {pages} page</p>
-//       <p>Free Bookmark Today: {freeBookmark ? 'yes' : 'no!'}</p>
-//     </section>
-//   );
-// };
+const Book = ({title = "[No Title Provided]", author="[No Author]", pages=0, freeBookmark}) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p>Pages: {pages} page</p>
+      <p>Free Bookmark Today: {freeBookmark ? 'yes' : 'no!'}</p>
+    </section>
+  );
+};
 
-// const Hiring = () => 
-//   <div>
-//     <p>The library is hiring. Go to www.library.com/jobs for more info!</p>
-//   </div>;
+const Hiring = () => 
+  <div>
+    <p>The library is hiring. Go to www.library.com/jobs for more info!</p>
+  </div>;
 
-// const NotHiring = () => 
-//   <div>
-//     <p>The library is not hiring. Check back later for more info!</p>
-//   </div>;
+const NotHiring = () => 
+  <div>
+    <p>The library is not hiring. Check back later for more info!</p>
+  </div>;
 
-// class Library extends Component {
+class Library extends Component {
 
-//   state = { 
-//     freeBookmark: true,
-//     open: true,
-//     hiring: true,
-//     data: [],
-//     loading: false
-//   }
-
-//   componentDidMount() {
-//     this.setState({loading: true});
-//     fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
-//       .then(data => data.json())
-//       .then(data => this.setState({data, loading: false}));
-//   }
-
-//   componentDidUpdate() {
-//     console.log("The component just updated!");
-//   }
-
-//   toggle = () => {
-//     this.setState(prevState => ({
-//         open: !prevState.open
-//     }));
-//   }
-
-//   render() {
-//     console.log(this.state);
-//     const {books} = this.props;
-//     return (
-//       <div>
-//         {this.state.hiring ? <Hiring /> : <NotHiring />}
-//         {this.state.loading 
-//           ? "loading..." 
-//           : <div>
-//               {this.state.data.map((product, i) => {
-//                 return (
-//                   <div key="i">
-//                     <h3>Library Product of the Week!</h3>
-//                     <h4>{product.name}</h4>
-//                     <img alt={product.name} src={product.image} height={100} />
-//                   </div>
-//                 )
-//               })}
-//           </div>
-//         }
-//         <h1>The library is {this.state.open ? 'open' : 'closed'}.</h1>
-//         <button onClick={this.toggle}>Change</button>
-//         {books.map((book, id) => {
-//           return(
-//             <Book 
-//              key={id} 
-//              title={book.title} 
-//              author={book.author}
-//              pages={book.pages} 
-//              freeBookmark={this.state.freeBookmark} />
-//           );
-//         })} 
-//       </div>
-//     );
-//   };
-// };
-
-class FavoriteColorForm extends React.Component {
-
-  state = { value: '' }
-
-  newColor = e => this.setState({
-    value: e.target.value
-  });
-
-  submit = e => {
-    console.log(`New Color: ${this.state.value}`);
-    e.preventDefault();
+  static defaultProps = {
+    books: [
+      {"title": "Tahoe Tales", "author": "Chet Whitley", "pages": 1000 }
+    ]
   };
 
-  render() {
-    return (
-      <form onSubmit={this.submit}>
-        <label>Favorite Color: 
-          <input type="color" 
-            onChange={this.newColor} />
-        </label>
-        <button>Submit</button>
-      </form>
-    );
+  state = { 
+    freeBookmark: true,
+    open: true,
+    hiring: true,
+    data: [],
+    loading: false
   }
-}
+
+  componentDidMount() {
+    this.setState({loading: true});
+    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+      .then(data => data.json())
+      .then(data => this.setState({data, loading: false}));
+  }
+
+  componentDidUpdate() {
+    console.log("The component just updated!");
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+        open: !prevState.open
+    }));
+  }
+
+  render() {
+    console.log(this.state);
+    const {books} = this.props;
+    return (
+      <div>
+        {this.state.hiring ? <Hiring /> : <NotHiring />}
+        {this.state.loading 
+          ? "loading..." 
+          : <div>
+              {this.state.data.map((product, i) => {
+                return (
+                  <div key="i">
+                    <h3>Library Product of the Week!</h3>
+                    <h4>{product.name}</h4>
+                    <img alt={product.name} src={product.image} height={100} />
+                  </div>
+                )
+              })}
+          </div>
+        }
+        <h1>The library is {this.state.open ? 'open' : 'closed'}.</h1>
+        <button onClick={this.toggle}>Change</button>
+        {books.map((book, id) => {
+          return(
+            <Book 
+             key={id} 
+             title={book.title} 
+             author={book.author}
+             pages={book.pages} 
+             freeBookmark={this.state.freeBookmark} />
+          );
+        })} 
+      </div>
+    );
+  };
+};
 
 render(
-  <FavoriteColorForm />,
+  <Library books={bookList} />,
   document.getElementById("root")
 );
 
